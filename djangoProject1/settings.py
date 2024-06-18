26975f01b353419e7bdc5057b08a254b868ce64d
SAI <sai552kk@gmail.com>
2024-06-18 12:22:40 +0530

    Adding tailwind to the Django project and creating a superuser

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "myApp",
+    "tailwind",
+    "theme",
+    "django_browser_reload",
 ]
+
+TAILWIND_APP_NAME = "theme"
+INTERNAL_IPS = ['127.0.0.1']
+
+NPM_BIN_PATH = '/usr/local/bin/npm'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -49,6 +57,9 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "djangoProject1.urls"
