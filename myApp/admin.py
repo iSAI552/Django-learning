8e75f4a68f27996ef25fb409a9375e67ba16ae30
SAI <sai552kk@gmail.com>
2024-06-18 17:02:09 +0530

    Handling Django Admin and Relationship models

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import HeroVariety
-# Register your models here.
+from .models import HeroVariety, HeroReview, SpecialPower, CountryPopularIn
 
-admin.site.register(HeroVariety)
+
+# Register your models here.
+class HeroReviewInline(admin.TabularInline):
+    model = HeroReview
+    extra = 2
+
+
+class HeroVarietyAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "date_added")
+    inlines = [HeroReviewInline]
+
+
+class CountryPopularAdmin(admin.ModelAdmin):
+    list_display = ("name", "continent")
+    filter_horizontal = ("heroes_popular",)
+
+
+class SpecialPowerAdmin(admin.ModelAdmin):
+    list_display = ("hero",)
+
+
+admin.site.register(HeroVariety, HeroVarietyAdmin)
+admin.site.register(SpecialPower, SpecialPowerAdmin)
+admin.site.register(CountryPopularIn, CountryPopularAdmin)
--- myApp/models.py
+++ myApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -19,3 +20,44 @@ class HeroVariety(models.Model):
 
     def __str__(self):
         return self.name
+
+
+#    One-to-Many relations
+class HeroReview(models.Model):
+    hero = models.ForeignKey(HeroVariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} review for {self.hero.name}"
+
+
+# Many-to-Many relations
+
+class CountryPopularIn(models.Model):
+    CONTINENT_TYPE_CHOICES = [
+        ("AS", "Asia"),
+        ("EU", "Europe"),
+        ("AF", "Africa"),
+        ("SA", "South America"),
+        ("NA", "North America"),
+        ("AT", "Antarctica"),
+        ("AU", "Australia")
+    ]
+    name = models.CharField(max_length=100)
+    continent = models.CharField(max_length=2 ,choices=CONTINENT_TYPE_CHOICES)
+    heroes_popular = models.ManyToManyField(HeroVariety, related_name="countries")
+
+    def __str__(self):
+        return self.name
+
+
+# One-to-One Relationship
+class SpecialPower(models.Model):
+    hero = models.OneToOneField(HeroVariety, on_delete=models.CASCADE, related_name='superPower')
+    power_strength = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"SpecialPower of {self.hero.name}"
